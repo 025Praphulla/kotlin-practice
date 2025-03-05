dc0f9b3486194c244a962ed4838fc7546ebe12ea
025Praphulla <023bscit025@sxc.edu.np>
2025-03-05 17:49:46 +0545

    Add names to list

--- app/src/main/java/com/example/tutorialapp/MainActivity.kt
+++ app/src/main/java/com/example/tutorialapp/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import com.example.tutorialapp.ui.theme.TutorialAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +32,40 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TutorialAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                var name by remember { mutableStateOf("") }
+                var names by remember { mutableStateOf(listOf<String>()) }
+                Box(
+                    modifier = Modifier
+                        .padding(20.dp)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp)
+
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
+                            OutlinedTextField(
+                                value = name,
+                                onValueChange = { text ->
+                                    name = text
+                                },
+                                textStyle = TextStyle(
+                                    color = Color.Black
+                                )
+                            )
+                            Button(onClick = {
+                                if (name.isNotBlank()) {
+                                    names = names + name
+                                    name = ""
+                                }
+                            }) { Text(text = "Add") }
+                        }
+                    }
+                    ListNames(names = names)
                 }
             }
         }
@@ -31,17 +73,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TutorialAppTheme {
-        Greeting("Android")
+fun ListNames(names: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn {
+        items(names) { currentName ->
+            Text(
+                text = currentName,
+                modifier = Modifier
+                    .padding(12.dp)
+                    .border(width = 2.dp, color = Color.Red)
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            )
+        }
     }
 }
